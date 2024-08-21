dfa9fb91ace6a2b3f216ab1ee68c79708ac4cb04
Srivalli <snc.srivalli1@gmail.com>
2024-08-21 10:13:49 +0530

    First commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void demo() {
+		System.out.println("Test1");
+	}
+}
---
+++ src/test/java/tests/Test2.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void demo() {
+		System.out.println("Test2");
+	}
+}
